f9622ca0550bc2d994574b76d91757fad9690a1f
Mauricio López <amauricio.lopez@gmail.com>
2024-12-23 10:17:54 -0400

    Initial Commit

---
+++ ios-course/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ios-course
+//
+//  Created by Andrés López Mayorga on 12/23/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ios-courseTests/ios_courseTests.swift
@@ -0,0 +1,17 @@
+//
+//  ios_courseTests.swift
+//  ios-courseTests
+//
+//  Created by Andrés López Mayorga on 12/23/24.
+//
+
+import Testing
+@testable import ios_course
+
+struct ios_courseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
